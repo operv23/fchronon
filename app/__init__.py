7cb88c141bb0231d5317f2572d68d60607d9501b
operv23 <hariko.storm@gmail.com>
2018-03-20 23:16:29 +0700

    microblog

--- app/__init__.py
+++ app/__init__.py
@@ -4,15 +4,21 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mail import Mail
+from flask_moment import Moment
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
+moment = Moment(app)
 login = LoginManager(app)
 login.login_view = 'login'
+bootstrap = Bootstrap(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
